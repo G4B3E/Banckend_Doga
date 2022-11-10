8ed45966b078933643a6a459a4a7a7f7744a8ae0
roli <roli@roli.roli>
2022-11-10 14:47:46 +0100

    commit

---
+++ src/Statute.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Statue = void 0; 
+class Statue {
+    constructor(title, price, year, height) {
+        this.title = title;
+        this.price = price;
+        this.year = year;
+        this.height = height;
+    }
+}
+exports.Statue = Statue;
